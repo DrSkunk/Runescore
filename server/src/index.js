fe8d92623f53a30e1b340863ae81d9c99607ecf9
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2019-04-11 15:53:17 +0200

    fix date

--- server/src/index.js
+++ server/src/index.js
@@ -7,9 +7,9 @@ async function main() {
   try {
     ['wh1tebird', 'zamorakyan'].forEach(async username => {
       const skills = (await rsapi.rs.hiscores.player(username)).skills;
-      console.log(skills);
-      skills.timestamp = Date.now();
+      skills.timestamp = admin.firestore.Timestamp.fromDate(new Date());
       db.collection(username).add(skills);
+      console.log('Added entry for ' + username);
     });
   } catch (e) {
     console.error('Error fetching data');
